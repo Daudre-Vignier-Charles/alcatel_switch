92c6ffd1bcba68b45dcfac9711c1137629f0af03
charles <charles@laptop-cdv.iris-systeme.home.arpa>
2024-06-09 00:03:59 +0200

    ALPHA nouveau fichier : lib/exceptions.py

---
+++ lib/exceptions.py
@@ -0,0 +1,9 @@
+class SwitchConfigLogicError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+class SwitchConfigTypeError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
